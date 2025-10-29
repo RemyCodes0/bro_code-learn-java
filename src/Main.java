5bd6b63078bc8643a200c13a402e9f8948c7a59d
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-10-29 21:47:40 +0200

    nested loop

--- src/Main.java
+++ src/Main.java
@@ -371,13 +371,45 @@ public class Main{
     // for( int i = 0; i<10 ; i++ ){
     //     System.out.println(i);
     // }
-    for(int i = 0; i<10; i++){
-        if(i ==3){
-            break;
-            // continue;
-        }
-        System.out.println(i);
+    // for(int i = 0; i<10; i++){
+    //     if(i ==3){
+    //         break;
+    //         // continue;
+    //     }
+    //     System.out.println(i);
+    // }
+
+    // for(int i =1; i<=3; i++){
+    //     for(int j =1; j<=9; j++){
+    //     System.out.print(j+ " ");
+    // }
+    // System.out.println();
+    // }
+    
+    int rows;
+    int columns;
+    char symbol;
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number of rows: ");
+    rows = scanner.nextInt();
+
+    System.out.println("Enter the number of columns: ");
+    columns = scanner.nextInt();
+
+    System.out.println("Enter the symbol to use: ");
+    symbol = scanner.next().charAt(0);
+
+
+    for(int i =0; i<rows; i++){
+ for(int j=0;  j< columns; j++){
+        System.out.print(symbol);
     }
+    System.out.println();
+    }
+   
+
+    scanner.close();
     }
 
 }
