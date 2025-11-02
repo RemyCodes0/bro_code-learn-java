a6cd8435e3333c1d9c99cc4bbe7e62795e4fd431
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-11-02 20:51:06 +0200

    First step of banking system project

--- src/Bank.java
+++ src/Bank.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double balance=10.99;
+        boolean isRunning = true;
+        int choice;
+
+        while (isRunning) {
+            System.out.println("*************************");
+
+        System.out.println("Banking PRogram");
+        System.out.println("*************************");
+
+        System.out.println("1. Show Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+        System.out.println("*************************");
+
+
+        System.out.println("Enter your choice(1-4)");
+        choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1 -> showBalance(balance);
+            case 2 -> System.out.println("DEPOSIT");
+            case 3 -> System.out.println("WITHDRAW");
+            case 4 -> isRunning = false;
+            default -> System.out.println("Invalid choice");
+
+        }
+        }
         
+        scanner.close();
+
+    }   
+
+    static void showBalance(double balance){
+        System.out.printf("$%.2f\n", balance);
     }
 }
