b6aa69861baa402cbe48e2ccd570902097ec03e7
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-11-10 22:48:36 +0200

    introduction to oop with java

---
+++ src/Car.java
@@ -0,0 +1,25 @@
+public class Car {
+    
+    String make = "Ford";
+    String model = "Mustang";
+    int year = 2025;
+    double price = 58000.99;
+    boolean isRunning = false;
+
+    void start(){
+        isRunning = true;
+        System.out.println("You start the engine");
+    }
+    void stop(){
+        isRunning = false;
+        System.out.println("You stopped the engine");
+    }
+
+    void drive(){
+        System.out.println("You drive the "+ model);
+    }
+
+    void break(){
+        System.out.println("You break "+ model);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -544,8 +544,26 @@ public class Main{
         //     System.out.println();
         // }
 
-        
+  
 
+    Car car = new Car();
+
+    // car.isRunning = true;
+    System.out.println(car.make);
+    System.out.println(car.year);
+    System.out.println(car.price);
+    System.out.println(car.isRunning);
+
+    System.out.println(car.model);
+    System.out.println(car.isRunning);
+    car.start();
+    System.out.println(car.isRunning);
+    car.stop();
+    System.out.println(car.isRunning);
+
+    Car car2 = new Car();
+    System.out.println(car.make + " "+ car.model);
+    System.out.println(car2.make + " "+ car2.model);
 
 
 
