5456b63acb16b0a54f70f51c4c542b21197b194c
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-11-15 13:40:10 +0200

    aggragation

--- src/Main.java
+++ src/Main.java
@@ -725,18 +725,24 @@ public class Main{
         // }
 
 
-        Car2 car2 = new Car2("Charger", "Yellow", 100000);
+    //     Car2 car2 = new Car2("Charger", "Yellow", 100000);
 
-       // car2.model = "corvette";
-       car2.setColor("Blue");
-        car2.setPrice(5000);
+    //    // car2.model = "corvette";
+    //    car2.setColor("Blue");
+    //     car2.setPrice(5000);
 
 
-        System.out.println(car2.getColor()+" " +car2.getModel()+" "+  car2.getPrice());
+    //     System.out.println(car2.getColor()+" " +car2.getModel()+" "+  car2.getPrice());
 
         
+        Book book1 = new Book("The Fellow of the Ring", 423);
+        Book book2 = new Book("The two towers", 352);
+        Book book3 = new Book("The return of the king", 416);
 
+        Book[] books = {book1, book2, book3};
 
+        Library library = new Library("NYC Public Library", 1897, books);
+        library.displayInfo();
 
     }
 
